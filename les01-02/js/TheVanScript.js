f3292cbcb59b710e99cb6b1afe847308be9e08a9
TVeegy <tibo.vg@gmail.com>
2019-02-24 16:02:04 +0100

    Img source and auto alt

--- les01-02/js/TheVanScript.js
+++ les01-02/js/TheVanScript.js
@@ -1,14 +1,70 @@
 (function() {
-    
     "use strict";
     
-    let icecreamFunction = function(parameter) {
+    let globalImages;
+    
+    // FUNCTIONS
+    let addTitles = function() {
+        let images = document.querySelectorAll("img");
+        globalImages = images;
+        for (let singleImage of images){
+            singleImage.title = singleImage.alt;
+        }
+        console.log(`${images.length} image(s) with new (copied) alt text.`);
+    }
+    
+    let state = true;
+    let disableImgSource = function() {
+        if (state)
+            for (let singleImage of globalImages){
+                singleImage.src = singleImage.src + "X";
+                console.log(singleImage.src);
+                state = false;
+            }
         
+        else
+            for (let singleImage of globalImages){
+                singleImage.src = singleImage.src.substr(0, singleImage.src.length-1);
+                state = true;
+            }
+            
+    }
+    
+    // TESTING FUNCTIONS
+    let testingSomeStuff = function() {
+        console.log("Starting test:")
+        // collection all images...
+        let images = document.querySelectorAll("img");
+        
+        // test on first entry
+        let image = images[0];
+        
+        // testing referencing
+        console.log(`\nImage title: ${image.title}`);
+        console.log(`Image alt text: ${image.alt}`);
+
+        console.log("\ncreating reference variables..");
+        console.log("setting title var to new value..");
+        let propTitle = image.title;
+        let propAlt = image.alt;
+        propTitle = "-test-";
+        
+        console.log(`Edited value of ref var (title): ${propTitle}`);
+
+        console.log(`\nImage title: ${image.title}`);
+        console.log(`Image alt text: ${image.alt}`);
     }
     
     // EVENTS
     window.addEventListener('load', function() {
-        icecreamFunction();
+        addTitles();
+        disableImgSource();
+        disableImgSource(false);
+        //testingSomeStuff();
+    });
+    
+    document.getElementById("TEST").addEventListener('click', function() {
+        disableImgSource();
     });
 })();
 
